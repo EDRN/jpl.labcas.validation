017060badd6fd09765af7ecfa906faf7077eb59d
Sean Kelly <kelly@seankelly.biz>
2026-08-05 18:40:13 -0500

    Support #54: add replace-event-id-column for PDAC scans

--- run-reports.sh
+++ run-reports.sh
@@ -20,7 +20,7 @@ solr=https://localhost:8984/solr/
 Lung_Team_Project_2="Images_Site_AvinPOWpghrek Images_Site_ldytNSGnHnrBQ Images_Site_NVRiRYzqspbvMw Images_Site_UB5yhu2StyPSLQ Images_Site_weRc6TUHvOru6A Images_Site_YGaeI0aN9IAeRA LTP2-Site6 LTP2-Site7"
 
 # Sites for Prostate_MRI
-Prostate_MRI="Images_Site_baiIJNZ2MBqvw Images_Site_c41ux70b3h6cow Images_Site_ElkuApuKkJXw2A Images_Site_ER13y8kBMpUKA Images_Site_kMBCjAelMw4Dw Images_Site_qfP7OH9pjawWGA Images_Site_rvIOs4uv8Rbfng Images_Site_uDUsCV9ikmtw Images_Site_x4xa7dK1fGEV8g PMRI-Site10"
+Prostate_MRI="Images_Site_qfP7OH9pjawWGA Images_Site_baiIJNZ2MBqvw Images_Site_c41ux70b3h6cow Images_Site_ElkuApuKkJXw2A Images_Site_ER13y8kBMpUKA Images_Site_kMBCjAelMw4Dw Images_Site_rvIOs4uv8Rbfng Images_Site_uDUsCV9ikmtw Images_Site_x4xa7dK1fGEV8g PMRI-Site10"
 
 # Sites for PDAC
 PDAC="BCM UPMC"
@@ -38,13 +38,12 @@ mkdir --parents reports
 # Marker
 date 1>&2
 
-# The two sites of PDAC
-echo "Running PDAC" 1>&2
-for site in $PDAC; do
-    echo "Running PDAC $site" 1>&2
-    .venv/bin/validate-dicom-files --new-data --output reports/Pre-diagnostic_PDAC_Images \
-        --site-id $site $base/Pre-diagnostic_PDAC_Images/$site
-    echo "PDAC $site done" 1>&2
+# Now Prostate_MRI
+echo "Running Prostate_MRI" 1>&2
+for site in $Prostate_MRI; do
+    echo "Running Prostate_MRI $site" 1>&2
+    .venv/bin/validate-dicom-files --url $solr --output reports/Prostate_MRI --subset $site $base/Prostate_MRI
+    echo "Prostate_MRI $site done" 1>&2
     clean_tmp
 done
 
@@ -53,17 +52,19 @@ echo "Running Lung_Team_Project_2" 1>&2
 for site in $Lung_Team_Project_2; do
     echo "Running Lung_Team_Project_2 $site" 1>&2
     .venv/bin/validate-dicom-files --url $solr --output reports/Lung_Team_Project_2 \
-	--subset $site $base/Lung_Team_Project_2
+        --subset $site $base/Lung_Team_Project_2
     echo "Lung_Team_Project_2 $site done" 1>&2
     clean_tmp
 done
 
-# Now Prostate_MRI
-echo "Running Prostate_MRI" 1>&2
-for site in $Prostate_MRI; do
-    echo "Running Prostate_MRI $site" 1>&2
-    .venv/bin/validate-dicom-files --url $solr --output reports/Prostate_MRI --subset $site $base/Prostate_MRI
-    echo "Prostate_MRI $site done" 1>&2
+# The two sites of PDAC
+echo "Running PDAC" 1>&2
+for site in $PDAC; do
+    echo "Running PDAC $site" 1>&2
+    .venv/bin/validate-dicom-files --replace-event-id-column PatientID \
+	--new-data --output reports/Pre-diagnostic_PDAC_Images \
+        --site-id $site $base/Pre-diagnostic_PDAC_Images/$site
+    echo "PDAC $site done" 1>&2
     clean_tmp
 done
 
